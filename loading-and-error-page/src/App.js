1fdb2492df8e910f3f5aa8a349cb4599ea2ec09d
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2021-09-28 17:54:33 -0700

    Did Stuff

--- loading-and-error-page/src/App.js
+++ loading-and-error-page/src/App.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-import Returns from './components/Returns'
+function App() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-const App = () => {
-    return (
-        <>
-            <Returns />
-            <div>
-            
-            </div>
-        </>
-    )
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const res = await fetch(
+        'https://pokeapi.co/api/v2/pokemon/ditto',
+      );
+      const json = await res.json();
+      setData(json.hits);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, [setData]);
+
+  return (
+    <React.Fragment>
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <ul>
+          {data.map(item => (
+            <li key={item.ObjectId}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
